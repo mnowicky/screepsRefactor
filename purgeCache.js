3cbe9c855de9d4dcf9c70df7f77dda4fa2a4588b
Matt Nowicky <56040330+mnowicky@users.noreply.github.com>
2022-08-31 15:01:44 -0400

    Add files via upload
    starting fresh

---
+++ purgeCache.js
@@ -0,0 +1,35 @@
+module.exports = {
+    purgeCreeps: function(){
+        for(let name in Memory.creeps){
+            if(Game.creeps[name] == undefined){
+                delete Memory.creeps[name];
+            }
+        }
+    },
+
+    purgeRooms: function(){
+        for(let room in Memory.rooms){
+            if(Game.rooms[room] == undefined){
+                delete Memory.rooms[room];
+            }
+        }
+    },
+
+    purgeSpawns: function(){
+        for(let spawn in Memory.spawns){
+            if(Game.spawns[spawn] == undefined){
+                delete Memory.spawns[spawn];
+            }
+        }
+    },
+
+    purgeAll: function(){
+        if(Game.time % 100 === 0){
+            this.purgeRooms();
+            this.purgeSpawns();
+        }
+        if(Game.time % 10 === 0){
+            this.purgeCreeps();
+        }
+    }
+}
